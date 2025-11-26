3c4b4096575a46e0116a2260576e26826d30e32c
chenxu-whisper <1070669767@qq.com>
2025-11-26 17:04:44 +0800

    添加窗口大小、键盘事件响应

--- README.md
+++ README.md
@@ -6,7 +6,7 @@
 > * https://study.xiaoe-tech.com/#/muti_index
 >
 > **所需库**
-> * [GLFW](https://www.glfw.org/) : 是一个专门针对OpenGL的C语言库,它提供了一些渲染物体所需需的最低限度的接口，如创建窗口、处理输入事件等。
+> * [GLFW](https://www.glfw.org/) : 个专门针对OpenGL的C语言库,它提供了一些渲染物体所需需的最低限度的接口，如创建窗口、处理输入事件等。
 > * [GLAD](https://glad.dav1d.de/) : 根据不同的OpenGL版本,它提供了不同的函数指针,可以在不同的平台上使用，如在Windows上使用OpenGL 4.5,在macOS上使用OpenGL 4.1等。
 > * [GLEW](https://glew.sourceforge.net/) : 是一个跨平台的C++库,它提供了OpenGL的扩展功能，如加载OpenGL扩展函数指针等。
 > * [GLM](https://glm.g-truc.net/0.9.9/index.html) : 是一个OpenGL的数学库,它提供了一些常用的数学操作,如向量、矩阵、四元数等，
--- src/main.cpp
+++ src/main.cpp
@@ -5,11 +5,32 @@
 #define WINDOW_WIDTH 800
 #define WINDOW_HEIGHT 600
 
+/* 窗口大小变化回调函数
+ *@param window: 窗口对象指针
+ *@param width: 窗口宽度
+ *@param height: 窗口高度
+ */
+void FrameBufferSizeCallback(GLFWwindow* window, int width, int height)
+{
+    std::cout << "Window size: " << width << ", " << height << std::endl;
+}
+
+/* 键盘输入回调函数
+ *@param key: 键盘键值
+ *@param scancode: 键盘扫描码
+ *@param action: 键盘操作类型(GLFW_PRESS, GLFW_RELEASE, GLFW_REPEAT)
+ *@param mods: 键盘修饰键(GLFW_MOD_SHIFT, GLFW_MOD_CONTROL, GLFW_MOD_ALT, GLFW_MOD_SUPER)
+ */
+void KeyCallback(GLFWwindow* window, int key, int scancode, int action, int mods)
+{
+
+}
+
 int main()
 {
     std::cout << "Hello, OpenGL!" << std::endl;
 
-    // ********** 创建窗体 **********
+    /* ********** 创建窗体 ********** */
     // 1. 初始化GLFW的基本环境
     glfwInit();
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4); // 主版本号
@@ -18,16 +39,18 @@ int main()
     // 2. 创建窗体对象
     GLFWwindow* window = glfwCreateWindow(WINDOW_WIDTH, WINDOW_HEIGHT, "GLRenderer", nullptr, nullptr); // 创建窗体对象
     glfwMakeContextCurrent(window); // 使当前线程的上下文成为指定的窗口上下文
-    // 3. 执行窗体循环
+    // 3. 事件响应
+    glfwSetFramebufferSizeCallback(window, FrameBufferSizeCallback); // 监听窗口大小变化事件
+    glfwSetKeyCallback(window, KeyCallback); // 监听键盘输入事件
+    // 4. 执行窗体循环
     while (!glfwWindowShouldClose(window)) // 检查是否需要关闭窗口
     {
         glfwSwapBuffers(window); // 交换缓冲区，将渲染结果显示在窗口上
         glfwPollEvents(); // 处理事件，如键盘输入、鼠标移动等
     }
-    // 4. 退出程序,清理GLFW环境,释放资源
+    // 5. 退出程序,清理GLFW环境,释放资源
     glfwDestroyWindow(window); // 销毁窗口对象
     glfwTerminate(); // 终止GLFW环境，释放资源
-    // ****************************************
 
     return 0;
 }
